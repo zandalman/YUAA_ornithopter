031cc6771bb010b288665950e1434ba6699d72f3
Zachary Andalman <zandalman@gmail.com>
2021-03-09 20:07:06 -0500

    For Kai

--- arduino/TestServo_Feb25.ino
+++ arduino/TestServo_Feb25.ino
@@ -1,20 +1,26 @@
 #include <Servo.h>
 
 float amplitude = 20; // degrees
-float frequency = 2; //Hz
+float frequency = 2; // frequency in Hz
+int servoPin = 12; // Servo pin number
 
 Servo wing;
 
 int startTime = 0;
 
+// To-do for Kai
+// Add nextPos function
+// Inputs are throttle (0-1), roll (ms), pitch (degrees), and yaw (degrees)
+// Outputs are wing positions (degrees)
+
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   startTime = millis();
-  wing.attach(2);  // change me!
+  wing.attach(servoPin);
 }
 
 void loop() {
-  int nextPos = amplitude * sin(frequency * 2*PI * (millis() - startTime)/1000) + 90;
+  int nextPos = amplitude * sin(frequency * 2*PI * (millis() - startTime)/1000) + 90; // Replace with function
   Serial.print((millis() - startTime)/1000);
   Serial.print(", ");
   Serial.println(nextPos);
