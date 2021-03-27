d04d8889b4924ef4e3fadc3fc03f4bf5ae1fb546
Kai Xu <Gigitoe@users.noreply.github.com>
2021-03-27 12:56:15 -0700

    Update Kai.ino

--- arduino/Kai.ino
+++ arduino/Kai.ino
@@ -1,41 +1,81 @@
-#include <Servo.h>
+#include <math.h>
 
-const int maxFrequency = 5;
-const int minLevelAngle = -10;
-const int maxLevelAngle = 10;
-const int rollPhaseShift = 45;
-const int minPitchAngle = -5;
-const int maxPitchAngle = 5;
-const int yawAngle = 5;
+
+// Tweak these --------------------
+const float maxFrequency = 5;
+const int zeroThrottleAngle = 15;
+const int minLevelAngle = -30;
+const int maxLevelAngle = 30;
+const int maxRollPhaseShift = 30;
+const int maxPitchAngle = 10;
+const int maxYawAngle = 10;
+// --------------------------------
+
+
+// Don't tweak these --------------
+float throttle;
+float roll;
+float pitch;
+float yaw;
 
 int startTime;
+const int delayTime = 15;
+int elapsedTime;
 
-struct {int left, right;} wings = {0, 0};
+float levelTheta;
+float leftTheta;
+float rightTheta;
 
-void updatePosition(float elapsedTime, float throttle, float roll, float pitch, float yaw) {
-  int levelAmplitude = maxLevelAngle - minLevelAngle;
-  int zeroOffset = 90 + minPitchAngle;
-  float frequency = throttle * maxFrequency;
-  float normalizedTime = elapsedTime / 1000;
-  float phaseShift = roll * rollPhaseShift / 720;
-  float pitchTerm = maxPitchAngle - pitch * (maxPitchAngle - minPitchAngle);
-  float yawTerm = yaw * yawAngle;
+struct pair {
+  int left;
+  int right;
+} wings;
+// --------------------------------
 
-  wings.left = levelAmplitude * sin(frequency * 2 * PI * (elapsedTime + phaseShift)) + zeroOffset + pitchTerm + yawTerm;
-  wings.right = levelAmplitude * sin(frequency * 2 * PI * (elapsedTime - phaseShift)) + zeroOffset + pitchTerm - yawTerm;
+
+void updatePosition(float throttle, float roll, float pitch, float yaw) {
+  if (throttle == 0) {
+    wings.left = zeroThrottleAngle;
+  }
+  
+  float deltaTheta = throttle * maxFrequency * ((float) delayTime / 1000) * 2 * PI;
+  levelTheta += deltaTheta;
+
+  // left if roll < 0; level if roll == 0; roll if pitch > 0
+  if (pitch < 0) {
+    leftTheta = levelTheta + roll * maxRollPhaseShift * PI / 180;
+    rightTheta = levelTheta;
+  } else if (pitch == 0) {
+    leftTheta = levelTheta;
+    rightTheta = levelTheta;
+  } else {
+    leftTheta = levelTheta;
+    rightTheta = levelTheta - roll * maxRollPhaseShift * PI / 180;
+  }
+
+  float amplitude = (float) (maxLevelAngle - minLevelAngle) / 2;
+
+  wings.left = amplitude * sin(leftTheta) - pitch * maxPitchAngle + yaw * maxYawAngle;
+  wings.right = amplitude * sin(rightTheta) - pitch * maxPitchAngle - yaw * maxYawAngle;
 }
+
 
 void setup() {
   Serial.begin(9600);
   startTime = millis();
-//  wing.attach(servoPin);
+
+  levelTheta = 0;
+
+  // Arbitrary values
+  throttle = 0.5;
+  roll = -0.34;
+  pitch = 0.75;
+  yaw = -0.6;
 }
 
+
 void loop() {
-  updatePosition((millis() - startTime) / 1000, 0.5, 0.25, 0.75, 0.25);
-  Serial.print(wings.left);
-  Serial.print(", ");
-  Serial.print(wings.right);
-  Serial.print("\n");
-  delay(15);
+  updatePosition(throttle, roll, pitch, yaw);
+  
+  delay(delayTime);
 }
